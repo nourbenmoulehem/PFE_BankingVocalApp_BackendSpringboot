218b8f16b414c47c6eb99e007b64a054e305fccb
Nour <nbenmoulehom@gmail.com>
2024-02-01 20:20:35 +0100

    initial commit

---
+++ vocalbanking/src/main/java/com/attijari/vocalbanking/VocalbankingApplication.java
@@ -0,0 +1,13 @@
+package com.attijari.vocalbanking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VocalbankingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VocalbankingApplication.class, args);
+	}
+
+}
---
+++ vocalbanking/src/test/java/com/attijari/vocalbanking/VocalbankingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.attijari.vocalbanking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VocalbankingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
