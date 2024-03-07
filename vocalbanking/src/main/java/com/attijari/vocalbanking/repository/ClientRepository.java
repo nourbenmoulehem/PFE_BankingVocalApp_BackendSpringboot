0acd902ddb699e3b9f9c71fb9ec1cffccd674c9c
nourbenmoulehem <nbenmoulehom@gmail.com>
2024-03-07 22:47:58 +0100

    Add a custom query to select client by cin and phoneNumber

--- vocalbanking/src/main/java/com/attijari/vocalbanking/repository/ClientRepository.java
+++ vocalbanking/src/main/java/com/attijari/vocalbanking/repository/ClientRepository.java
@@ -3,9 +3,13 @@ package com.attijari.vocalbanking.repository;
 import com.attijari.vocalbanking.model.Client;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+
+import java.util.Date;
 import java.util.Optional;
 
 @Repository
 public interface ClientRepository extends JpaRepository<Client, Long> {
     Client findByCin(String cin);
+
+    Optional<Client> findByCinAndPhoneNumber(String cin, String phoneNumber);
 }
