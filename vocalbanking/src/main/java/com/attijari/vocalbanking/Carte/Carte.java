c8ab0b66e2dca72231e1c4bd1e2cb360f985c3fc
nourbenmoulehom <nbenmoulehom@gmail.com>
2024-03-24 18:22:58 +0100

    Create a OneToOne rel between Carte and Compte

--- vocalbanking/src/main/java/com/attijari/vocalbanking/Carte/Carte.java
+++ vocalbanking/src/main/java/com/attijari/vocalbanking/Carte/Carte.java
@@ -1,21 +1,23 @@
 package com.attijari.vocalbanking.Carte;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import com.attijari.vocalbanking.CompteBancaire.CompteBancaire;
+import com.fasterxml.jackson.annotation.JsonIdentityInfo;
+import com.fasterxml.jackson.annotation.ObjectIdGenerators;
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 import java.util.Date;
+import java.util.Random;
 
 @Data
 @Entity
 @Builder
 @NoArgsConstructor
 @AllArgsConstructor
+@JsonIdentityInfo(generator= ObjectIdGenerators.IntSequenceGenerator.class, property="id_produit")
 public class Carte {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -25,6 +27,23 @@ public class Carte {
     private String status;
     private Date date_expiration;
     private int plafond;
-    // @OneToOne // one-to-one relationship with CompteBancaire
+    @OneToOne(mappedBy = "carte")
+    private CompteBancaire compteBancaire;
+
+    @PrePersist
+    public void prePersist() {
+        this.numero_carte = generateCarte();
+    }
+
+    public static String generateCarte() {
+        // Generate 17 random digits
+        StringBuilder sb = new StringBuilder();
+        Random random = new Random();
+        for (int i = 0; i < 15; i++) {
+            sb.append(random.nextInt(10)); // Generates a random digit (0-9)
+        }
+        // Concatenate bank identifier with the random digits
+        return sb.toString();
+    }
 
 }
