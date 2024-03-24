a7a5dd4e71b633082e489acdaea3f15dfc7cc928
nourbenmoulehom <nbenmoulehom@gmail.com>
2024-03-24 18:24:44 +0100

    update CodeOffre enum

--- vocalbanking/src/main/java/com/attijari/vocalbanking/Carte/CodeOffre.java
+++ vocalbanking/src/main/java/com/attijari/vocalbanking/Carte/CodeOffre.java
@@ -1,7 +1,19 @@
 package com.attijari.vocalbanking.Carte;
 
+import lombok.RequiredArgsConstructor;
+
 public enum CodeOffre {
-    CODE_006, // westart generalisee
-    CODE_007, // wetrust generalisee
-    CODE_003, // carte technologique
+    CODE_006("006"), // westart generalisee
+    CODE_007("007"), // wetrust generalisee
+    CODE_003("003"); // carte technologique
+
+    private final String code;
+
+    CodeOffre(String code) {
+        this.code = code;
+    }
+
+    public String getCode() {
+        return code;
+    }
 }
