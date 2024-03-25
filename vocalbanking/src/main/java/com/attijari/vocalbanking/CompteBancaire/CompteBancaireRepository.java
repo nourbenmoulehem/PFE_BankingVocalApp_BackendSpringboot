b56782c5beea70d04ab37ca6922bdc82cc4c45ed
nourbenmoulehom <nbenmoulehom@gmail.com>
2024-03-25 18:47:54 +0100

    Committing CompteBancaireRepository.java

--- vocalbanking/src/main/java/com/attijari/vocalbanking/CompteBancaire/CompteBancaireRepository.java
+++ vocalbanking/src/main/java/com/attijari/vocalbanking/CompteBancaire/CompteBancaireRepository.java
@@ -8,4 +8,5 @@ import org.springframework.stereotype.Repository;
 public interface CompteBancaireRepository extends JpaRepository<CompteBancaire, Long> {
 
     boolean existsByRIB(String rib);
+
 }
