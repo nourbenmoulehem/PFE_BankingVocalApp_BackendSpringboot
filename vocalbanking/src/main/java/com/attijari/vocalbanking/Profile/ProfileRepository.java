e816354f717c71bf98d128dcb2d8e7c8afdb4e1a
nourbenmoulehom <nbenmoulehom@gmail.com>
2024-04-12 20:02:03 +0100

    add a custom query to fetch by clientId

--- vocalbanking/src/main/java/com/attijari/vocalbanking/Profile/ProfileRepository.java
+++ vocalbanking/src/main/java/com/attijari/vocalbanking/Profile/ProfileRepository.java
@@ -2,6 +2,7 @@ package com.attijari.vocalbanking.Profile;
 
 import com.attijari.vocalbanking.Profile.Profile;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
@@ -9,4 +10,7 @@ import java.util.Optional;
 @Repository
 public interface ProfileRepository extends JpaRepository<Profile, Long> {
     Optional<Profile> findByEmail(String email);
+
+    @Query("SELECT p FROM Profile p WHERE p.client.clientId = ?1")
+    Profile findByClientId(Long client_id);
 }
