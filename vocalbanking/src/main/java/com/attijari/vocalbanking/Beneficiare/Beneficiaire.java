a7cf6819e80b795829615da1e8d7ed0d2ceec3ee
nourbenmoulehom <nbenmoulehom@gmail.com>
2024-04-12 15:16:31 +0100

    add JsonIdentityInfo to avoid reccurency

--- vocalbanking/src/main/java/com/attijari/vocalbanking/Beneficiare/Beneficiaire.java
+++ vocalbanking/src/main/java/com/attijari/vocalbanking/Beneficiare/Beneficiaire.java
@@ -2,6 +2,8 @@ package com.attijari.vocalbanking.Beneficiare;
 
 import com.attijari.vocalbanking.Client.Client;
 import com.attijari.vocalbanking.Virement.Virement;
+import com.fasterxml.jackson.annotation.JsonIdentityInfo;
+import com.fasterxml.jackson.annotation.ObjectIdGenerators;
 import jakarta.persistence.*;
 import lombok.*;
 
@@ -12,6 +14,7 @@ import java.util.List;
 @Builder
 @NoArgsConstructor
 @AllArgsConstructor
+@JsonIdentityInfo(generator= ObjectIdGenerators.PropertyGenerator.class, property = "id", scope= Beneficiaire.class) // This is used to avoid infinite recursion
 public class Beneficiaire {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
